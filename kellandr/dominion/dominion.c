c082e2b915de5cb2a9d5af85af0af980e31a7b09
Andrius Kelly <kellandr@flip1.engr.oregonstate.edu>
2017-11-19 14:46:11 -0800

    change discardCard() to move card from handPos and into discardPile. Does not add card to playedCard. Also adds input validation

--- kellandr/dominion/dominion.c
+++ kellandr/dominion/dominion.c
@@ -1264,37 +1264,27 @@ discardCard(handPos, currentPlayer, state, 0);
 int discardCard(int handPos, int currentPlayer, struct gameState *state, int trashFlag)
 {
 	
-  //if card is not trashed, added to Played pile 
-  if (trashFlag < 1)
-    {
-      //add card to played pile
-      state->playedCards[state->playedCardCount] = state->hand[currentPlayer][handPos]; 
-      state->playedCardCount++;
-    }
-	
-  //set played card to -1
-  state->hand[currentPlayer][handPos] = -1;
-	
+	//input validation
+	if(handPos < 0 || handPos >= state->handCount[currentPlayer] ){
+		return 1;
+	}
+
+	//if !trashFlag, add to discardpile
+	if(!trashFlag){
+		int discardCount = state->discardCount[currentPlayer];
+		state->discard[currentPlayer][discardCount] = state->hand[currentPlayer][handPos];
+		state->discardCount[currentPlayer] += 1;
+	}
+
+
   //remove card from player's hand
-  if ( handPos == (state->handCount[currentPlayer] - 1) ) 	//last card in hand array is played
-    {
-      //reduce number of cards in hand
-      state->handCount[currentPlayer]--;
-    }
-  else if ( state->handCount[currentPlayer] == 1 ) //only one card in hand
-    {
-      //reduce number of cards in hand
-      state->handCount[currentPlayer]--;
-    }
-  else 	
-    {
-      //replace discarded card with last card in hand
-      state->hand[currentPlayer][handPos] = state->hand[currentPlayer][ (state->handCount[currentPlayer] - 1)];
-      //set last card to -1
-      state->hand[currentPlayer][state->handCount[currentPlayer] - 1] = -1;
-      //reduce number of cards in hand
-      state->handCount[currentPlayer]--;
-    }
+  state->hand[currentPlayer][handPos] = state->hand[currentPlayer][ (state->handCount[currentPlayer] - 1)];
+  
+  //set last card to -1
+  state->hand[currentPlayer][ state->handCount[currentPlayer] - 1 ] = -1;
+  
+  //reduce number of cards in hand
+  state->handCount[currentPlayer] -= 1;    
 	
   return 0;
 }
